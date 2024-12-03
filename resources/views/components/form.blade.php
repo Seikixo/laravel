698e3901184dbb2b12ec986f8844057d6ced436f
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-12-03 12:02:44 +0800

    update component styles

---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,3 @@
+<div class="flex w-[50%] h-auto border border-solid rounded-lg border-black p-6">
+    {{$slot}}
+</div>
--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -3,18 +3,17 @@
 @section('title', 'Edit Page')
 
 @section('content')
-    <div>
-        <form class="grid grid-cols-2 grid-rows-1 w-[50%] h-auto justify-center items-center border border-solid rounded-lg border-black gap-4 p-4" method="POST" action=" {{ route('students.update', $student->id) }}">
+    <x-form>
+        <form  method="POST" action=" {{ route('students.update', $student->id) }}">
             @csrf
             @method('PUT')
             <div class="col-start-1 col-end-1 row-start-1 row-end-1 gap-2">
                 <input class="border border-solid rounded-md border-black p-2 mb-2" type="text" name="name" id="name" value="{{ old('name', $student->name) }}">
                 <input class="border border-solid rounded-md border-black p-2 mb-2" type="text" name="section" id="section" value="{{ old('section', $student->section) }}">
                 <input class="border border-solid rounded-md border-black p-2 mb-2" type="text" name="year" id="year" value="{{ old('year', $student->year) }}">
-                <button class="flex flex-col justify-center items-center w-auto h-auto bg-blue-100 hover:bg-blue-300" type="submit" >Submit</button>
             </div>
                 
-            <div class="col-start-2 col-end-2 row-start-1 row-end-1 gap-2">
+            <div class="col-start-2 col-end-2 row-start-1 row-end-1 gap-2 mt-4">
                 @foreach ($student->grades as $grade)
                     @foreach (['english', 'math', 'science', 'history'] as $subject)
                         <input 
@@ -26,9 +25,11 @@
                     @endforeach               
                 @endforeach
             </div>
+            <div class="flex flex-row gap-4">
+                <button class="flex flex-col justify-center items-center w-18 h-8 border rounded-md p-4 mt-4 bg-blue-100 hover:bg-blue-300" type="submit">Submit</button>
+                <button class="flex flex-col justify-center items-center w-18 h-8 border rounded-md p-4 mt-4 bg-blue-100 hover:bg-blue-300" action="{{route('students.index')}}">Cancel</button>
+            </div>
             
-        
         </form>
-    </div>
-
+    </x-form>
 @endsection
